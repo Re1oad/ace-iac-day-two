d3671a979f9071914a9bd0e6dfedfc20e0c522b7
Re1oad <88439627+Re1oad@users.noreply.github.com>
2022-01-10 14:59:44 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
-    # organization = "Golden-Sand-Corp>"
+    organization = "Golden-Sand-Corp>"
     workspaces {
       name = "ace-iac-day-two"
     }
